ee15e50f0d41b8928ababee32281624b1ddc60ed
Sylvie-B <unsolo@hotmail.fr>
2022-01-26 10:04:10 +0100

    envoi de mail

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php
 if(isset($_POST['send'], $_POST['targetAdress'], $_POST['message'])){
-    mail($_POST['targetAdress'], 'newsletter', $_POST['message'], 'heliosens59@gmail.com');
+    echo mail($_POST['targetAdress'], 'newsletter', $_POST['message'], 'heliosens59@gmail.com') ?
+        "succès" : "Echec";
 }
-
 ?>
 <!doctype html>
 <html lang="fr">
@@ -11,16 +11,19 @@ if(isset($_POST['send'], $_POST['targetAdress'], $_POST['message'])){
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>formulaire d'envoi</title>
 </head>
 <body>
 
     <form action="index.php" method="post">
-        <input type="email" name="targetAdress">
-        <textarea name="message" id="message" cols="30" rows="10"></textarea>
+
+        <input type="email" name="targetAdress" placeholder="votre mail">
+        <div>
+            <textarea name="message" id="message" cols="30" rows="10" placeholder="votre texte"></textarea>
+        </div>
         <input type="submit" name="send">
     </form>
 
 </body>
 </html>
-<?php
+
